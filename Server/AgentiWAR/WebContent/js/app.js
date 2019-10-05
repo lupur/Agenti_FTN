be357250051fe06c57a4d6500fd08be57ac8813c
Sava Maric <sava.maric@cyrillicsoftware.net>
2019-10-05 20:08:00 +0200

    client ws

--- Server/AgentiWAR/WebContent/js/app.js
+++ Server/AgentiWAR/WebContent/js/app.js
@@ -1,4 +1,48 @@
-var app = angular.module("agentClient", ["ngRoute"]);
+'use strict';
+var app = angular.module("agentClient", ['ngRoute', 'angular-websocket']);
+
+app.run(function ($websocket) {
+	
+	var dataStream = $websocket('ws://echo.websocket.org');
+	
+    dataStream.onMessage(function(message) {
+    	console.log(message);
+      });
+//    var ws = $websocket.$new('ws://localhost:8080/AgentiWAR/agentClasses'); // instance of ngWebsocket, handled by $websocket service
+//
+//    ws.$on('$open', function () {
+//        console.log('Oh my gosh, websocket is really open! Fukken awesome!');
+//
+//        ws.$emit('ping', 'hi listening websocket server'); // send a message to the websocket server
+//
+//        var data = {
+//            level: 1,
+//            text: 'ngWebsocket rocks!',
+//            array: ['one', 'two', 'three'],
+//            nested: {
+//                level: 2,
+//                deeper: [{
+//                    hell: 'yeah'
+//                }, {
+//                    so: 'good'
+//                }]
+//            }
+//        };
+//
+//        ws.$emit('pong', data);
+//    });
+//
+//    ws.$on('pong', function (data) {
+//        console.log('The websocket server has sent the following data:');
+//        console.log(data);
+//
+//        ws.$close();
+//    });
+//
+//    ws.$on('$close', function () {
+//        console.log('Noooooooooou, I want to have more fun with ngWebsocket, damn it!');
+//    });
+});
 
 app.config(['$routeProvider', function($routeProvider) {
 	  $routeProvider
